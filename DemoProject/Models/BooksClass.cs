5e31dee75583166a1c81f6421555581b7e49fa64
Sid <siddharth@Sids-MacBook-Pro.local>
2020-05-17 19:45:00 +0530

    fix

---
+++ DemoProject/Models/BooksClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoProject.Models
+{
+    public class Books
+    {
+
+        [Key]
+        public int Key { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+    }
+}
--- DemoProject/Startup.cs
+++ DemoProject/Startup.cs
@@ -35,11 +35,12 @@ namespace DemoProject
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+            //added for hot reload!
 
 
         }
-
+        // MIDDLEWARE PIPELINE ,THE ORDER OF MIDDLEWARE MATTERS!
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
